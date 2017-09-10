fae54abbcf6f7a77e8193ba63ef88029cd796ff5
Anderson <anderson.nobrega62@gmail.com>
2017-09-10 19:45:19 -0300

    Suporte a criação de banco SQLite3 e adicionado funções de armazenamento e leitura do banco de dados

--- analise_musica.py
+++ analise_musica.py
@@ -1,7 +1,7 @@
 try:
     from scipy.io import wavfile
-    from scipy.signal import spectrogram
-    from scipy.stats import iqr
+    from scipy.signal import spectrogram, periodogram
+    import sqlite3
     import numpy as np
     import os
     import matplotlib.pyplot as plt
@@ -11,19 +11,24 @@ except ImportError:
     print("Import Error")
     quit(1)
 
-def ler_musica(musica):
+conn = sqlite3.connect("musicas.db")
+cursor = conn.cursor()
+
+def ler_musica(musica, formato):
     """Le os arquivos no formato mp3 e converter para wav mono channel"""
 
-    subprocess.call(['ffmpeg', '-i', '%s.mp3' %musica,'-ac', '1','-ab', '320k','%s.wav' %musica])
+    subprocess.call(['ffmpeg', '-i', '%s%s' %(musica, formato),'-ac', '1','-ab', '320k','%s.wav' %musica])
 
-def guardar_info(valor_medio, musica="Desconhecida"):
-    """Guarda os dados da musica em um csv"""
+def inicializa_tabela():
+    cursor.execute("CREATE TABLE IF NOT EXISTS musicas (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, musica TEXT, valor FLOAT)")
 
-    arquivo_csv = open("info.csv", "a")
-    if os.stat("info.csv").st_size == 0:
-        arquivo_csv.write("Música,Valores")
-    arquivo_csv.write("\n" + musica + "," + str(valor_medio))
-    arquivo_csv.close()
+def inserir_dados(nova_musica, novo_valor):
+    cursor.execute("INSERT INTO musicas(musica, valor) VALUES(?, ?)", (nova_musica, float(novo_valor)))
+    conn.commit()
+
+def fechar_banco():
+    cursor.close()
+    conn.close()
 
 def plotar(spectogram, times, frequencies):
     """Plota o spectrograma da música"""
@@ -39,6 +44,8 @@ def analise_musica(musica):
     sample_rate, samples = wavfile.read('%s.wav' %musica)
 
     frequencies, times, spectogram = spectrogram(samples, sample_rate)
+
+    #f, pxx = periodogram(samples, sample_rate)
 
     return np.mean(spectogram)
 
@@ -61,39 +68,43 @@ def guardar_comparacoes(lista, nome_musica):
             arquivo.write("\n%s = %.2f" %(lista[i][0], lista[i][1]))
 
 def ler_dados(valor_comparar, nome_musica):
-    "Recebe o valor de uma música e compara com todas que estão no dataset"
+    "Recebe o valor de uma música e compara com todas que estão no banco de dados"
 
-    arquivo = pd.read_csv("info.csv")
-    dataset = pd.DataFrame(arquivo)
+    cursor.execute('SELECT * FROM musicas')
     valores = []
-    for i in range(len(dataset["Valores"])):
-        valor = comparar_musica(valor_comparar, dataset["Valores"][i])
-        valores.append([dataset["Música"][i], valor])
+    for linha in cursor.fetchall():
+        valor = comparar_musica(valor_comparar, linha[2])
+        valores.append([linha[1], valor])
     valores.sort(key=lambda x: x[1], reverse=True)
     guardar_comparacoes(valores, nome_musica)
+
+def imprimir_dados():
+    cursor.execute('SELECT * FROM musicas')
+    for linha in cursor.fetchall():
+        print(linha)
 
 def apagar_musicas():
     """Apaga as musicas de formato .wav na pasta atual"""
 
     for file in os.listdir(os.getcwd()):
-        if file.endswith(".wav"):
+        if (file.endswith(".wav")) or (file.endswith(".mp3")) or (file.endswith(".flac")):
             os.remove(file)
 
 def criar_banco_musica():
     """Le todas as músicas na pasta atual e guarda o valor de cada uma"""
 
     for file in os.listdir(os.getcwd()):
-        if file.endswith(".mp3"):
-            nome_musica = os.path.splitext(os.path.basename(file))[0]
-            ler_musica(nome_musica)
+        if (file.endswith(".mp3")) or (file.endswith(".flac")):
+            nome_musica, formato_musica = os.path.splitext(os.path.basename(file))
+            ler_musica(nome_musica, formato_musica)
             spec1 = analise_musica(nome_musica)
-            guardar_info(spec1, nome_musica)
+            inserir_dados(nome_musica, spec1)
     apagar_musicas()
 
 def recomendar_musicas(musica):
     """Recebe uma música e busca no banco de dados as mais parecidas"""
 
-    ler_musica(musica)
+    ler_musica(musica, ".mp3")
     spec1 = analise_musica(musica)
     ler_dados(spec1, musica)
     apagar_musicas()
